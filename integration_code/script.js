76173e09d4d2aac72bd59a730d1e85cf27773077
Prakhar Tiwari <indianstudentexplolearner@gmail.com>
2023-05-10 13:46:39 +0530

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- integration_code/script.js
+++ integration_code/script.js
@@ -12,32 +12,37 @@ const gfr_feedback_ch_counter = document.getElementById("gfr_feedback_ch_counter
 
 
 
-
-gfr_main_div.style.display = "none";
-gfk_overlay.style.display = "none";
+// OPENING AND CLOSING THE RATING BOX
+// gfr_main_div.style.display = "none";
+// gfk_overlay.style.display = "none";
 
 
 gfr_button.addEventListener('click', ()=>{
 
-    gfr_main_div.style.display = "flex";
-    gfk_overlay.style.display = "flex";
+    // gfk_overlay.style.display = "flex";
+    // gfr_main_div.style.display = "flex";
+    gfr_main_div.showModal();
+
 })
 
 
 close_gfr_main_div.addEventListener('click', ()=>{
 
-    gfr_main_div.style.display = "none";
-    gfk_overlay.style.display = "none";
+    // gfk_overlay.style.display = "none";
+    // gfr_main_div.style.display = "none";
+    gfr_main_div.close();
+
 })
 
 
 
+// Feedback 
 feedback_area.addEventListener("keyup", ()=>{
     current_length = feedback_area.value.length;
     gfr_feedback_ch_counter.innerText = `${current_length}/150`
     if (current_length>120) {
         gfr_feedback_ch_counter.style.color = "red"
-    }
+    }   
     if (current_length < 120 && (gfr_feedback_ch_counter.style.color == "red")) {
         gfr_feedback_ch_counter.style.color = "black"
         
@@ -47,5 +52,26 @@ feedback_area.addEventListener("keyup", ()=>{
 
 
 
+const stars = document.querySelectorAll('.star');
+let selectedRating = 0;
 
+function handleStarClick(event) {
+    const starId = event.target.id;
 
+    for (let i = 0; i < stars.length; i++) {
+        const star = stars[i];
+        if (star.id <= starId) {
+            star.classList.add('selected');
+        } else {
+            star.classList.remove('selected');
+        }
+    }
+
+    selectedRating = starId.replace('star', '');
+    console.log('Selected rating:', selectedRating);
+}
+
+for (let i = 0; i < stars.length; i++) {
+    const star = stars[i];
+    star.addEventListener('click', handleStarClick);
+}
