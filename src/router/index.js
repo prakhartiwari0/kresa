8d5ad51d361cda621d64d9895a70e9960442d9b5
Prakhar Tiwari <indianstudentexplolearner@gmail.com>
2023-05-16 17:48:00 +0530

    Create basic Dashboard Files & route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import HowitworksView from '../views/HowitworksView.vue'
+import HomeView from '../views/MainSiteViews/HomeView.vue'
+import HowitworksView from '../views/MainSiteViews/HowitworksView.vue'
+
+import MainDashView from '../views/DashboardViews/MainDashView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +16,12 @@ const router = createRouter({
       path: '/howitworks',
       name: 'howitworks',
       component: HowitworksView
-    }
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: MainDashView
+    },
   ]
 })
 
